fb8147eb58f153f846fe637b5bbce0dfda17b0b4
woodylucas <wlucas0311@gmail.com>
2020-11-14 21:49:02 -0500

    added functionality to a form

---
+++ src/NewTodoForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+class NewTodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { task: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    this.props.createTodo(this.state);
+    this.setState({ task: "", id: uuidv4() });
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="task">New Todo</label>
+        <input
+          type="text"
+          placeholder="New Todo"
+          id="task"
+          value={this.state.task}
+          name="task"
+          onChange={this.handleChange}
+        />
+        <button>Add Todo</button>
+      </form>
+    );
+  }
+}
+
+export default NewTodoForm;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
 import Todo from "./Todo";
+import NewTodoForm from "./NewTodoForm";
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: [{ task: "Walk the fish" }, { task: "Groom Chickens" }],
+      todos: [],
     };
+    this.create = this.create.bind(this);
+  }
+  create(newTodo) {
+    this.setState((currState) => ({
+      todos: [...currState.todos, newTodo],
+    }));
   }
   render() {
-    const todos = this.state.todos.map((todo) => <Todo task={todo.task} />);
+    const todos = this.state.todos.map((todo) => (
+      <Todo key={todo.id} id={todo.id} task={todo.task} />
+    ));
     return (
       <div>
         <h1>Todo List!</h1>
+        <NewTodoForm createTodo={this.create} />
         <ul>{todos}</ul>
       </div>
     );
